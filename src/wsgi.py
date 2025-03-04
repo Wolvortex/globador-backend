5aad5f27cca14c2dcb326a8fa166087839479890
Omar Hatem <wolvortex@gmail.com>
2025-03-04 23:05:47 +0200

    Debugged Settings for PythonAnyWhere

--- src/wsgi.py
+++ src/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'src.settings.development')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'src.development')
 
 application = get_wsgi_application()
